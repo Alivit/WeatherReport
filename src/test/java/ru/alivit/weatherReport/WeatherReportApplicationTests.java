1dcfd20e353a4534fe474ec9f4b0e67aaa584ef7
Alivit <vltal241201@gmail.com>
2023-11-30 20:45:39 +0300

    init repository

---
+++ src/test/java/ru/alivit/weatherReport/WeatherReportApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alivit.weatherReport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherReportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
